ff2405da45fadc175e79c9103af6ae9234d2a019
Ubuntu <ubuntu@ip-172-31-37-252.ec2.internal>
2021-05-18 06:02:21 +0000

    Create S3 Bucket
    - Added a stack to create S3 bucket that will store artifacts to host application

--- stack-helper.sh
+++ stack-helper.sh
@@ -2,3 +2,6 @@
 
 # Create IAM user with
 aws cloudformation create-stack --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --stack-name iam-stack --template-body file://iam-user-stack.yml
+
+# Create an S3 bucket for the project
+aws cloudformation update-stack --stack-name s3-stack --template-body file://s3-stack.yml
