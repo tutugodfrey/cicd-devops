e04b164233146cbbf4296a5751a3f1cb6f5ef990
Ubuntu <ubuntu@ip-172-31-37-252.ec2.internal>
2021-05-18 07:11:32 +0000

    Update Cloud front
    - Update cloud front stack to use outputs from S3 stack

--- stack-helper.sh
+++ stack-helper.sh
@@ -5,3 +5,6 @@ aws cloudformation create-stack --capabilities "CAPABILITY_IAM" "CAPABILITY_NAME
 
 # Create an S3 bucket for the project
 aws cloudformation update-stack --stack-name s3-stack --template-body file://s3-stack.yml
+
+# Create cloud front stack
+aws cloudformation create-stack --stack-name cloudfront-stack --template-body file://cloudfront.yml
