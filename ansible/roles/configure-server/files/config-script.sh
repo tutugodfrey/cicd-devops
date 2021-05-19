585939dea534aa2c4439146439b3710859ce26e0
Ubuntu <ubuntu@ip-172-31-37-252.ec2.internal>
2021-05-19 12:35:27 +0000

    Run Migrations
    - Add job to run migrations
    - Update ansible configuration - use environment module
    - Map circle environment to ansible tasks template

--- ansible/roles/configure-server/files/config-script.sh
+++ ansible/roles/configure-server/files/config-script.sh
@@ -1,23 +1,13 @@
 #! /bin/bash
 
 apt update -y;
-# Export environment variables
-export ENVIRONMENT=production
-export TYPEORM_CONNECTION=postgres
-export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
-export TYPEORM_HOST=
-export TYPEORM_PORT=5532
-export TYPEORM_ENTITIES=
-export TYPEORM_USERNAME=TYPEORM_USERNAME
-export TYPEORM_PASSWORD=${TYPEORM_PASSWORD}
-export TYPEORM_DATABASE=
-
-apt install -y gcc make;
+apt install -y gcc make git;
 cd ~
 # curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
-curl -sL https://deb.nodesource.com/setup_15.x -o nodesource_setup.sh
+curl -sL https://deb.nodesource.com/setup_15.x -o nodesource_setup.sh;
 bash nodesource_setup.sh
-
 apt install nodejs -y;
+npm install pm2 -g;
 
-npm install pm2 -g
+# Clone the project repository
+git clone https://github.com/tutugodfrey/cicd-devops /home/ubuntu/cicd-devops;
