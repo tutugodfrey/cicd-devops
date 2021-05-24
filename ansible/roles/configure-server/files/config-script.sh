91df2f493f6149f245f1077f2fa2019421eaf0f8
Ubuntu <ubuntu@ip-172-31-37-252.ec2.internal>
2021-05-24 13:23:17 +0000

    Fix Backend Deploy
    - Add only necessary files to build artifacts
    - Archive artifacts to be copy to server with Ansible

--- ansible/roles/configure-server/files/config-script.sh
+++ ansible/roles/configure-server/files/config-script.sh
@@ -3,8 +3,7 @@
 apt update -y;
 apt install -y gcc make git;
 cd ~
-# curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
-curl -sL https://deb.nodesource.com/setup_15.x -o nodesource_setup.sh;
+curl -sL https://deb.nodesource.com/setup_13.x -o nodesource_setup.sh;
 bash nodesource_setup.sh
 apt install nodejs -y;
 npm install pm2 -g;
